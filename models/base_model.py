33ccdfdac07919afad99f367efd2e4716d182433
Allen Nicholson <ranicho2@gmail.com>
2021-03-29 16:44:09 -0500

    Update to get rid of sa_instance_state

--- models/base_model.py
+++ models/base_model.py
@@ -46,13 +46,13 @@ class BaseModel:
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
+        if '_sa_instance_state' in self.__dict__.keys():
+            del self.__dict__['_sa_instance_state']
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary.keys():
-            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
