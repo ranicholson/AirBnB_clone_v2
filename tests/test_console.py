d3d660c42ff09bd8ff12982e4f9d6b4e195734d9
faspen <2335@holbertonschool.com>
2021-03-30 16:47:33 +0000

    added missing tests

---
+++ tests/test_console.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Test module for console
+"""
+import unittest
+
+
+class test_console(unittest.TestCase):
+    """Tests"""
+    
---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Test database storage
+"""
+import unittest
+
+
+class test_db_storage(unittest.TestCase):
+    """Tests"""
+
