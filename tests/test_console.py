711753561fb5b71cbd15fd55aa6d4ee15abe6d72
faspen <2335@holbertonschool.com>
2021-03-31 15:56:58 +0000

    more tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,8 +2,21 @@
 """Test module for console
 """
 import unittest
+import console
+import pep8
 
 
 class test_console(unittest.TestCase):
+
     """Tests"""
-    
+
+    def test_pep8(self):
+        """Test pep8"""
+        style = pep8.StyleGuide(quiet=True)
+        check = style.check_files(['console.py'])
+        self.assertEqual(check.total_errors, 0, "needs fixes")
+
+    def test_doc(self):
+        """Test docstring"""
+        self.assertIsNot(console.HBNBCommand.__doc__, None)
+        self.assertTrue(len(console.HBNBCommand.__doc__) >= 1)
--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -2,8 +2,28 @@
 """Test database storage
 """
 import unittest
+from models import storage
+from models.engine.db_storage import DBStorage
+import pep8
 
 
 class test_db_storage(unittest.TestCase):
+
     """Tests"""
 
+    def test_pep8(self):
+        """Test pep8 formatting"""
+        style = pep8.StyleGuide(quiet=True)
+        check = style.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(check.total_errors, 0, "needs fixes")
+
+    def test_attrs(self):
+        """Test for attrs"""
+        self.assertTrue(hasattr(DBStorage, 'all'))
+        self.assertTrue(hasattr(DBStorage, 'new'))
+        self.assertTrue(hasattr(DBStorage, 'save'))
+        self.assertTrue(hasattr(DBStorage, 'delete'))
+        self.assertTrue(hasattr(DBStorage, 'reload'))
+        self.assertTrue(hasattr(DBStorage, 'close'))
+        self.assertTrue(hasattr(DBStorage, '_DBStorage__engine'))
+        self.assertTrue(hasattr(DBStorage, '_DBStorage__session'))
