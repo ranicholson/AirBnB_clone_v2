aad2d3adfc87d3972f509caa00df50d0b097eaaf
ranicholson <ranicho2@gmail.com>
2021-04-15 18:16:34 +0000

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,43 +1,14 @@
 #!/usr/bin/env bash
-# Prepare for web static
+# Script to configure server
 
+sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data
-sudo mkdir -p /data/web_static
-sudo mkdir -p /data/web_static/releases
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases/test
-
-printf "<html>
-	    <head>
-	    </head>
-	    <body>
-	    Holberton School
-	    </body>
-	</html>" > sudo tee /data/web_static/releases/test/index.html
-
-FILE=/data/web_static/current
-
-if [ -f "$FILE" ]
-then
-    sudo rm /data/web_static/current
-    sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-else
-    sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-fi
-
-sudo chgrp -R ubuntu /data/
-sudo chown -R ubuntu /data/
-printf %s "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	add_header X-Served-By $HOSTNAME;
-	root /var/www/html;
-	index index.html index.htm;
-	location /hbnb_static/ {
-		alias /data/web_static/current/;
-		autoindex off;
-	}
-}" | sudo tee /etc/nginx/sites-available/default
-
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+echo -e "<html>\n<body>\n<h1>Howdy!<h1>\n</body>\n</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -i '/:80 default_server/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx reload
 sudo service nginx restart
