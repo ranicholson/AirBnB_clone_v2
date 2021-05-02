dc56311215483a1b88318c8edb32472c3a9713e5
ranicholson <ranicho2@gmail.com>
2021-05-02 18:39:38 +0000

    Task 0

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that starts a flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Prints hello hbnb"""
+    return "Hello HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Prints hekoo hbnb"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
